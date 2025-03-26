3bdf86fd3c1448f8c93271f1b4e5d6e83191e8ce
Anubhav Bajpai <anubhavbajpai910@gmail.com>
2025-03-26 08:34:56 +0530

    Add files via upload

---
+++ currency-exchange-service/target/classes/data.sql
@@ -0,0 +1,10 @@
+insert into currency_exchange
+(conversion_multiple, id, currency_from, currency_to, environment)
+values(65, 1000, 'USD', 'INR', '');
+insert into currency_exchange
+(conversion_multiple, id, currency_from, currency_to, environment)
+values(80, 1002, 'EUR', 'INR', '');
+insert into currency_exchange
+(conversion_multiple, id, currency_from, currency_to, environment)
+values(70, 1001, 'INR', 'INR', '');
+ 
